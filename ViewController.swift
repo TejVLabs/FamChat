bb3331fd193e3f4d7d1ca9bdda0b7a9383163905
Tejaswi Vemulapati <tejvlabs@gmail.com>
2017-03-27 21:57:28 -0700

    Create ViewController.swift

---
+++ ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var proceedToChat:UIButton!
+    @IBOutlet var editFamily:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
